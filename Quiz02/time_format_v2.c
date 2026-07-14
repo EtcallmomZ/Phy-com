e0fc7d8bddbbbffaccc37cc596acc27de6057015
EtcallmomZ <eiweiw321@gmail.com>
2026-07-14 21:38:06 +0700

    add version2

--- Quiz02/time_format_v2.c
+++ Quiz02/time_format_v2.c
@@ -4,14 +4,11 @@ int main()
 {
 
     int hour,miniute;
-    char time[5];
 
     scanf("%d",&hour);
     scanf("%d",&miniute);
 
-    sprintf(time,"%02d:%02d",hour,miniute);
-
-    printf("%02d-%02d",time);
+    printf("%02d:%02d",hour,miniute);
 
 
 
