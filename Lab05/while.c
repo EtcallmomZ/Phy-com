bc02dc144825ac2b2ef5752d9d1da5d5812e94dd
EtcallmomZ <eiweiw321@gmail.com>
2026-07-31 14:20:18 +0100

    add Lab05.4

---
+++ Lab05/while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num,i;
+
+    scanf("%d",&num);
+    i = 1;
+    while(i <= num)
+    {
+        printf("%d ", i);
+        i++;
+        
+    }
+    
+    return 0;
+}
