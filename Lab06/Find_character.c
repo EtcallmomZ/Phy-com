bb9ff17c3c668d0da66e6affead8457e78995076
EtcallmomZ <eiweiw321@gmail.com>
2026-08-08 14:39:30 +0100

    add Lab06.5

---
+++ Lab06/Find_character.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+
+    char text[301];
+    char alpha,ans;
+    int count = 0;
+
+    scanf("%[^\n]",text);
+    scanf(" %c",&alpha);
+
+    ans = tolower(alpha);
+
+
+    int position[301];
+
+    for (int i = 0 ; i < 301 ; i++)
+    {
+        if (tolower(text[i]) == ans)
+        {
+           position[count] = i + 1;
+           count++;
+        }
+        
+    }
+
+    if(count > 0){
+        printf("There is/are %d \"%c\" in the above sentences.\n",count,ans);
+        printf("Position: ");
+        for(int k = 0 ; k < count ; k++)
+        {
+            printf("%d",position[k]);
+    
+            if (k < count - 1)
+            {
+                printf(", ");
+            }
+        }
+    }
+    else
+    {
+        printf("Not found.");
+    }
+
+    return 0;
+}
