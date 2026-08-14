aca2fb52dffd3ff5b04f69199852ff262debb3c1
EtcallmomZ <eiweiw321@gmail.com>
2026-08-14 07:50:34 +0100

    add lab07.5

---
+++ Lab07/math_function.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+double getVaule()
+{
+    double vaule;
+    scanf("%lf",&vaule);
+    return vaule;
+}
+
+double findMin(double array[],int size)
+{
+    double min = array[0];
+    for (int i = 1 ; i < size; i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double findMax(double array[],int size)
+{
+   double max = array[0];
+   for (int i = 1 ; i < size; i++)
+    {
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max; 
+    
+}
+
+double findAvg(double array[],int size)
+{
+   double avg = 0;
+   for (int i = 0 ; i < size ; i++)
+   {  
+     avg += array[i];
+   }
+   return avg / size;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    double array[n];
+    for (int i = 0 ; i < n ;i++)
+    {
+        array[i] = getVaule();
+    }
+
+    double min , max ,avg;
+    min = findMin(array,n);
+    max = findMax(array,n);
+    avg = findAvg(array,n);
+
+
+    printf("%d Values\n" , n);
+    printf("Min: %.3lf\n" ,min);
+    printf("Max: %.3lf\n" ,max);
+    printf("Avg: %.3lf\n" ,avg);
+    
+
+    return 0;
+}
